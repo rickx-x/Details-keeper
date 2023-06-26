2d359a3eba26b124a29a62b99259720d1aa95d22
rickx-x <singhrks700@gmail.com>
2023-06-26 22:04:28 +0530

    ready to run application for saving details on the go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
+  const [form, setForm] = useState({ name: '', email: '' });
+  const [data, setData] = useState([]);
+
+  const addData = () => {
+    setData([...data, form]);
+    setForm({name:'',email:''});
+  }
+  const removeItem = (index)=>{
+    let arr =data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+
+      {/* form input */}
+      <div className='form'>
+        <Stack direction="row" spacing={2}>
+          <TextField value={form.name} onChange={(event) => {
+            setForm({...form,name:event.target.value})
+          }}
+            id="outlined-basic" label="name" variant="outlined" />
+          <TextField value={form.email} onChange={(event) => {
+            setForm({...form,email:event.target.value})
+          }}
+            id="outlined-basic" label="email" variant="outlined" />
+          <Button onClick={addData} variant="contained" color="success">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+
+      {/* data output from form */}
+      <div className='data'>
+        <div className='dataField'>
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+
+        {
+          data.map((element, index) => {
+            return (
+              <div className='dataField'>
+                <h4>{element.name}</h4>
+                <h4>{element.email}</h4>
+                <Button onClick={()=>removeItem(index)} variant="contained" color="error"><DeleteIcon /></Button>
+              </div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>All Records</h1>
+    </div>
+  )
+}
+
+export default Header
